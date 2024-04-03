a32bbae8e5087e22cd2527f933bf14f85df8f5e2
JeppeSandberg <97033099+JeppeSandberg@users.noreply.github.com>
2024-04-03 18:40:40 +0200

    commit

--- my-ecommerce-app/client/src/Services/api.js
+++ my-ecommerce-app/client/src/Services/api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = 'http://localhost:3001';
 
 export function getCategories() {
   return axios.get(`${BASE_URL}/categories`);
